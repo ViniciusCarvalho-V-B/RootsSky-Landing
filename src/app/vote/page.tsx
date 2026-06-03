18cee458ec016b419e45e640f6c9cc8058142f1b
Vinicius <viniciuscvb3@gmail.com>
2026-06-03 02:41:25 -0300

    Fix ESLint error: remove unused Image import in vote page

--- src/app/vote/page.tsx
+++ src/app/vote/page.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
-
 export const metadata = {
   title: "Vote no RootsSky - Ajude o Servidor",
   description: "Vote no RootsSky e ganhe recompensas épicas dentro do jogo!",
